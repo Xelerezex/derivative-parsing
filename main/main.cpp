8562f1b07493a9e0b096aca35594379b78ca9eb5
Xelerezex <nikitavalderman@gmail.com>
2023-03-19 14:04:24 +0300

    Added main folder with main.cpp.

---
+++ main/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "token.hpp"
+
+#define UNUSED(x) (void)x;
+
+int main(int argc, char *argv[])
+{   
+    UNUSED(argc);
+    UNUSED(argv);
+
+    std::cout << "programm  works correctly" << std::endl;
+}
